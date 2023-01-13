db606918ad9f2e9f7b8b620a3dbb3b20e158a4cd
Isaac Gonzalez <84966379+IsaacAGonzalez@users.noreply.github.com>
2023-01-12 21:15:06 -0800

    learning handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,6 +19,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
+      // layout: 'userdashboard'
       // projects, 
       // logged_in: req.session.logged_in 
     });
