bd900c3c6a6e9e44658beda40bbdd9ed401838c7
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-01-16 21:04:06 -0800

    consolidated login/signup and have initial footer...will have it finalized tomorrow

--- public/js/login.js
+++ public/js/login.js
@@ -22,27 +22,28 @@ const loginFormHandler = async (event) => {
   }
 };
 
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
+const signupFormHandler = async (event) => {
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+  // Gather the data from the form elements on the page
+  const username = document.querySelector('#username-input-signup').value.trim();
+  const email = document.querySelector('#email-input-signup').value.trim();
+  const password = document.querySelector('#password-input-signup').value.trim();
+  const zipcode = document.querySelector('#zipcode-input-signup').value.trim();
 
-//   const name = document.querySelector('#name-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
-
-//   if (name && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'POST',
-//       body: JSON.stringify({ name, email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
+  if (signup) {
+      const response = await fetch('/api/user', {
+          method: 'POST',
+          body: JSON.stringify({ username, email, password, zipcode  }),
+          headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+          document.location.replace('/dashboard');
+      } else {
+          alert('Failed to log in');
+      }
+  }
+};
 
 document
   .querySelector('.login-form')
