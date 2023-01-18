753c8d36d96d71d44a6e5d4bc7da39f92ffba65e
Louis <louyoon89@gmail.com>
2023-01-18 15:51:29 -0800

    organize route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,7 +38,7 @@ router.get('/login', async (req, res) => {
   }
 });
 
-router.get('/partials/signup', async (req, res) => {
+router.get('/signup', async (req, res) => {
   try {
     res.render('signup', {
     });
