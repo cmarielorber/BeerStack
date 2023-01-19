e36c2545e62761694d45e716d5232ce61a8868f3
Louis <louyoon89@gmail.com>
2023-01-18 19:39:04 -0800

    signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+
+  }
+})
+
 router.post('/signup', async (req, res) => {
+  console.log(req.body)
   try {
     const userData = await User.create(req.body);
-
+    console.log("insideasdfafa")
     req.session.save(() => {
       req.session.first_name = userData.first_name;
       req.session.last_name = userData.last_name;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,35 @@
+// const { response } = require("express");
 
 const signupFormHandler = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    // event.preventDefault();
-    // Gather the data from the form elements on the page
-    const firstname = document.querySelector('#first-input-signup').value.trim();
-    const lastname = document.querySelector('#last-input-signup').value.trim();
-    const email = document.querySelector('#email-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
-  
-    if (signup) {
-        const response = await fetch('', {
-            method: 'POST',
-            body: JSON.stringify({ firstname, lastname, email, password  }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to log in');
-        }
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  // event.preventDefault();
+  // Gather the data from the form elements on the page
+  const first_name = document.querySelector('#first-input-signup').value.trim();
+  const last_name = document.querySelector('#last-input-signup').value.trim();
+  const email = document.querySelector('#email-input-signup').value.trim();
+  const password = document
+    .querySelector('#password-input-signup')
+    .value.trim();
+
+const bodyText = JSON.stringify({ first_name, last_name, email, password  })
+console.log(bodyText)
+
+  if (event) {
+
+    const response = await fetch('api/users/signup', {
+        method: 'POST',
+        body: bodyText,
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to log in');
     }
-  };
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
