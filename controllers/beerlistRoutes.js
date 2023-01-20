f20eac1331b0e01cc150c6cc13a82030c7cfd56b
Louis <louyoon89@gmail.com>
2023-01-20 11:14:31 -0800

    routesupdate

--- controllers/beerlistRoutes.js
+++ controllers/beerlistRoutes.js
@@ -1,3 +1,6 @@
+const router = require('express').Router();
+const Beers = require('../models/Beers');
+
 router.get('/', async (req, res) => {
   const beerData = await Beers.findAll().catch((err) => { 
       res.json(err);
@@ -5,3 +8,6 @@ router.get('/', async (req, res) => {
       const beers = beerData.map((beer) => beer.get({ plain: true }));
       res.render('beerpost', { beers });
     });
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboard-routes');
+const beerlistRoutes = require('./beerlistRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
