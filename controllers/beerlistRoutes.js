3a846387384ab9b1530e833ad162448802d5abb5
Louis <louyoon89@gmail.com>
2023-01-20 12:25:02 -0800

    route fix

--- controllers/beerlistRoutes.js
+++ controllers/beerlistRoutes.js
@@ -2,30 +2,29 @@ const router = require('express').Router();
 const Beers = require('../models/Beers');
 
 router.get('/', async (req, res) => {
-  const beerData = await Beers.findAll().catch((err) => { 
-      res.json(err);
-    });
-      const beers = beerData.map((beer) => beer.get({ plain: true }));
-      res.render('beerpost', { beers });
-    });
+  const beerData = await Beers.findAll().catch((err) => {
+    res.json(err);
+  });
+  const beers = beerData.map((beer) => beer.get({ plain: true }));
+  res.render('beerpost', { beers });
+});
 
+// MODIFY TO SORT BY ALPHA
+router.get('/beerpostalpha', async (req, res) => {
+  const beerData = await Beers.findAll().catch((err) => {
+    res.json(err);
+  });
+  const beers = beerData.map((beer) => beer.get({ plain: true }));
+  res.render('beerpostalpha', { beers });
+});
 
-    // MODIFY TO SORT BY ALPHA
-    router.get('/beerpostalpha', async (req, res) => {
-      const beerData = await Beers.findAll().catch((err) => { 
-          res.json(err);
-        });
-          const beers = beerData.map((beer) => beer.get({ plain: true }));
-          res.render('beerpostalpha', { beers });
-        });
-  
+// MODIFY TO SORT BY LIKES
+router.get('/beerpostlikes', async (req, res) => {
+  const beerData = await Beers.findAll().catch((err) => {
+    res.json(err);
+  });
+  const beers = beerData.map((beer) => beer.get({ plain: true }));
+  res.render('beerpostlikes', { beers });
+});
 
-        // MODIFY TO SORT BY LIKES
-    router.get('/beerpostnumber', async (req, res) => {
-      const beerData = await Beers.findAll().catch((err) => { 
-          res.json(err);
-        });
-          const beers = beerData.map((beer) => beer.get({ plain: true }));
-          res.render('beerpostalpha', { beers });
-        });
 module.exports = router;
