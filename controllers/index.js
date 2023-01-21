fa02a9d6abae847c79ab38e934f396315d133224
Isaac Gonzalez <84966379+IsaacAGonzalez@users.noreply.github.com>
2023-01-21 11:37:39 -0800

    reimplemented lost css and formatting

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboard-routes');
 const beerlistRoutes = require('./beerlistRoutes')
+const homepageRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/beerpost', beerlistRoutes);
+router.use('/homepage', homepageRoutes);
 
 module.exports = router;
