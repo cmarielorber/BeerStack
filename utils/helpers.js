d2be897d8dc86df4df73447862f4bec5ff960fff
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2023-01-21 21:26:08 -0800

    dashboard posts/comments

--- utils/helpers.js
+++ utils/helpers.js
@@ -20,3 +20,16 @@ module.exports = {
     }
   },
 };
+
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`
+        }
+
+        return word;
+    }
+};
