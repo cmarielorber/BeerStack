d4e066a0288d9a55fddf9b2eb3915e45123ac66a
Isaac Gonzalez <84966379+IsaacAGonzalez@users.noreply.github.com>
2023-01-23 00:42:04 -0800

    post data visible in dashboard, just need to make it look good now

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,19 +5,27 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, async (req, res) => {
   try {
     const login_status = req.session.logged_in;
-    const userData = await User.findByPk(req.session.user_id, {
+    const currentUser = req.session.user_id;
+    const userData = await User.findByPk(currentUser, {
       attributes: {
         exclude: ['password']
       }
     });
+
+    const userRawPosts = await Post.findAll({ where: { user_id: currentUser} });
+    const userPosts = [];
+    for (let i = 0; i < userRawPosts.length; i++) {
+      const singlePost = userRawPosts[i].dataValues;
+      userPosts.push(singlePost);
+    };
+
     const coolerList = userData.cooler;
     const coolerData = [];
     for (let i = 0; i < coolerList.length; i++) {
       const singleBeer = await Beers.findByPk(coolerList[i]);
       coolerData.push(singleBeer.dataValues);
-    }
-    console.log(coolerData)
-    res.render('dashboard', { login_status, coolerData });
+    };
+    res.render('dashboard', { login_status, coolerData, userPosts });
   } catch (err) {
     res.status(500).json(err);
   }
