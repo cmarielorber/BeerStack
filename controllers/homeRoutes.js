50af063f8bcad692413db448d42a9881af141d89
louyoon89 <113860554+louyoon89@users.noreply.github.com>
2023-01-23 13:55:54 -0800

    beers on home (#84)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,28 +1,28 @@
 const router = require('express').Router();
 const { Model } = require('sequelize');
-const { User, Post } = require('../models');
+const { User, Beers, Post } = require('../models');
 // const Beers = require('../models/Beers');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    const login_status = req.session.logged_in;
-    const postData = await Post.findAll().catch((err) => {
-      res.json(err);
-    });
-    const posts = postData.slice(0,5).map((post) => post.get({ plain: true }));
-    res.render('homepage', { posts , login_status });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  const login_status = req.session.logged_in;
+  const postData = await Post.findAll().catch((err) => {
+    res.json(err);
+  });
+  const beerData = await Beers.findAll().catch((err) => {
+    res.json(err);
+  });
 
+  const posts = postData.slice(0, 10).map((post) => post.get({ plain: true }));
+  const beers = beerData.slice(0, 10).map((post) => post.get({ plain: true }));
+  res.render('homepage', { posts, beers, login_status });
+});
 
 // Render Socal Brew
 router.get('/socalbrew', async (req, res) => {
   try {
     const login_status = req.session.logged_in;
-    res.render('socalbrew', {  login_status });
+    res.render('socalbrew', { login_status });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,6 +44,5 @@ router.get('/signup', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
